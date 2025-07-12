08eddf111e5a71e9aca58235d3a8b301e810dbaa
Andre <sebastianvh86@gmail.com>
2025-07-12 17:49:33 -0600

    First commit

---
+++ src/main/java/org/sebas/blogbackendspringboot/BlogBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.sebas.blogbackendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBackendSpringbootApplication.class, args);
+    }
+
+}
